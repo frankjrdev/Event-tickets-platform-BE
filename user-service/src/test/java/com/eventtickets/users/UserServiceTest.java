a20afaa0528ddd5d970c6fbd2af8c0639302a775
Francisco Quintero <frankjrdev@gmail.com>
2025-06-26 09:04:04 +0200

    Base for microservices and apigateway project

---
+++ user-service/src/test/java/com/eventtickets/users/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.eventtickets.users;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserServiceTest {
+    @Test
+    void exampleTest() {
+        assertTrue(true, "This test should pass");
+    }
+}
